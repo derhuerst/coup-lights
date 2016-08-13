c484e78a0f0fd82de950d72da1b9e8fd50adb4c2
Jannis R <mail@jannisr.de>
2016-08-13 04:47:33 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 'use strict'
 
-// todo
+const qs = require('querystring')
+const http = require('http')
+
+
+
+const lights = (color) => new Promise((yay, nay) => {
+	const req = http.request({
+		hostname: '192.168.2.4',
+		port:     80,
+		method:   'POST',
+		path:     '/change_color',
+		headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+	}, (res) => {
+		if (res.statusCode < 200 || res.statusCode >= 300)
+			nay(`status code ${res.statusCode}, connecting to 192.168.2.4:80.`)
+		else yay('succes')
+	})
+
+	req.on('error', (err) =>
+		nay(`${err.code} error, connecting to 192.168.2.4:80.`))
+
+	req.end(qs.stringify({color}))
+})
+
+module.exports = lights
