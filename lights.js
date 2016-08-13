cc9f34a0cec9268ef483c1f2e4a10e13b5201b43
Jannis R <mail@jannisr.de>
2016-08-13 04:47:59 +0200

    executable :wrench:

---
+++ lights.js
@@ -0,0 +1,25 @@
+#!/usr/local/bin/node
+'use strict'
+
+const minimist = require('minimist')
+const random = require('random-color')
+
+const lights = require('./index')
+
+const argv = minimist(process.argv.slice(2))
+
+if (argv.h || argv.help) {
+	process.stdout.write(`\
+Usage: coup-lights [color]
+
+color must be in hex format (e.g. 'ffcc00').
+If you don't pass a color, a random one will be picked.\n`)
+	process.exit(0)
+}
+
+const color = argv._[0] || random().hexString().slice(1)
+lights(color)
+.catch((msg) => {
+	process.stderr.write(msg + '\n')
+	process.exit(1)
+})
