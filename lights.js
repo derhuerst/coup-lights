dedd9397d427a805fe0b3e954d38f59e0f4ebc9f
Jannis R <mail@jannisr.de>
2016-11-03 20:11:33 +0100

    better help text
    fixes #1

--- lights.js
+++ lights.js
@@ -12,10 +12,22 @@ if (argv.h || argv.help) {
 	process.stdout.write(`\
 Usage: coup-lights [color]
 
-color can be 6 digits ('ffcc00'), 3 digits ('fc0') or something like 'red'.
-If you don't pass a color, a random one will be picked.\n`)
+color can be 6 digits ('#ffcc00'), 3 digits ('#fc0') or something like 'red'.
+Remember to put the color in quote marks (' or "), otherwise your shell will
+treat is as a comment.
+
+If you don't pass a color, a random one will be picked.
+
+Examples:
+	coup-lights
+	coup-lights '#ffcc77'
+	coup-lights green
+`)
 	process.exit(0)
 }
+
+console.error('argv', argv);
+process.exit(0);
 
 const color = argv._[0] || random().hexString().slice(1)
 lights(color)
